04d94c05272e63b47ddba0e00ef0fee8924f2e43
Aldmi <Aldmi@bk.ru>
2018-06-05 22:15:34 +0700

    Add "MarkerEndAction" in ActionFromCashier

--- src/Server/Actions/ActionFromCashier.cs
+++ src/Server/Actions/ActionFromCashier.cs
@@ -10,8 +10,8 @@ namespace Server.Actions
     {
         #region prop
 
-        public CashierHandling CashierHandling { get; }
-        public Func<Task> CashierAct { get; }
+        public CashierHandling CashierHandling { get; } //????
+        private Func<Task> CashierAct { get; }
 
         #endregion
 
@@ -27,6 +27,65 @@ namespace Server.Actions
         }
 
         #endregion
+
+
+
+        /// <summary>
+        /// Маркер завершения задачи
+        /// Успешное завершение возвращается null
+        /// Еслт ошибка возвращается Exception
+        /// </summary>
+        private TaskCompletionSource<Exception> _tcs;
+        public Task<Exception> MarkerEndAction()
+        {
+            _tcs = new TaskCompletionSource<Exception>();
+            return _tcs.Task;
+        }
+
+
+        /// <summary>
+        /// Обертка над задачей CashierAct
+        /// Выполянть на очереди.
+        /// </summary>
+        public async Task Invoke()
+        {
+            try
+            {
+                await CashierAct();
+                _tcs.TrySetResult(null);
+            }
+            catch (Exception ex)
+            {
+                _tcs.TrySetResult(ex);
+               // throw;
+            }        
+        }
+
+
+        //private TaskCompletionSource<bool> _tcs;
+        //private Task<bool> PlayWithControl()
+        //{
+        //    _tcs = new TaskCompletionSource<bool>();
+        //    Task.Run(async () =>
+        //    {
+        //        if (_currentCallId != null)
+        //        {
+        //            try
+        //            {
+        //                _praesideoOi.startCreatedCall(_currentCallId.Value);
+        //                await Task.Delay(_timeResponse);
+        //                _tcs.TrySetResult(false);
+        //            }
+        //            catch (Exception)
+        //            {
+        //                _tcs.TrySetResult(false);
+        //            }
+        //        }
+        //    });
+
+        //    return _tcs.Task;
+        //}
+
 
     }
 }
